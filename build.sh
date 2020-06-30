1852caf9d18c03aed09f6f4aee09b85b8ce42fd0
abattagl <abattagl@localhost>
2020-06-30 09:44:19 +0200

    Added scripts for automated build Added placeholders to endpoint connection

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 ./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
 docker build -f src/main/docker/Dockerfile.native -t quay.io/qiot/qiot-service .
 docker push quay.io/qiot/qiot-service
+#docker run -it --rm -p 8080:8080 --net host quay.io/qiot/qiot-service
