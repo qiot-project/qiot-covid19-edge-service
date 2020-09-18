09ae3823a6d2340c0a9c02e6b0c0012e0ed38b4d
abattagl <abattagl@ROVERF.fritz.box>
2020-09-18 09:55:53 +0200

    added tag to docker build command Quarkus bom upgraded to 1.8.0.Final

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 ./mvnw clean package -Pnative  -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiot/qiot-service --force
-docker build -f src/main/docker/Dockerfile.native -t quay.io/qiot/qiot-service .
-docker push quay.io/qiot/qiot-service
+docker rmi quay.io/qiot/qiot-service:1-x86_64 --force
+docker build -f src/main/docker/Dockerfile.native -t quay.io/qiot/qiot-service:1-x86_64 .
+docker push quay.io/qiot/qiot-service:1-x86_64
 #docker run -it --rm -p 8080:8080 --net host quay.io/qiot/qiot-service
